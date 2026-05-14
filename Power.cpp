2d69902520f5afd3de833b51605859ec265a987e
Adithya Sripada <166884730+Adithya-Sripada@users.noreply.github.com>
2026-05-13 22:38:53 -0700

    Update Power.cpp

--- Power.cpp
+++ Power.cpp
@@ -11,7 +11,7 @@ public:
         }
         if(n <= 0) return 1;
         if(n == 1) return x;
-        if(n%2 == 0) return x * pow(x, n-1);
-        else return x * pow(x*x, n/2);
+        if(n%2 == 0) return x * myPow(x, n-1);
+        else return x * myPow(x*x, n/2);
     }
 };
