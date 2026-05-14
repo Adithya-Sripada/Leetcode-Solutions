9663efcab231e137263fac47c697987fbde153d1
Adithya Sripada <166884730+Adithya-Sripada@users.noreply.github.com>
2026-05-13 22:37:54 -0700

    Add files via upload

---
+++ LinkedListCycleII.cpp
@@ -0,0 +1,27 @@
+#include <map>
+
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode(int x) : val(x), next(nullptr) {}
+ };
+
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(head == nullptr || head->next == nullptr) return nullptr;
+        std::map<ListNode*, int> m;
+        ListNode* node = head;
+        while(node) {
+            try {
+                int n = m.at(node);
+                return node;
+            }
+            catch(const std::out_of_range& ex) {
+                m.insert(std::pair<ListNode*, int>{node, 1});
+            }
+            node = node->next;
+        }
+        return nullptr;
+    }
+};
---
+++ Power.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n == -2147483648) {
+            if(x > 1) return 0;
+            else return 1;
+        }
+        else if(n < 0) {
+            n = -n;
+            x = 1/x;
+        }
+        if(n <= 0) return 1;
+        if(n == 1) return x;
+        if(n%2 == 0) return x * pow(x, n-1);
+        else return x * pow(x*x, n/2);
+    }
+};
