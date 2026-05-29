ce9d7e9b05a6f511d0d2560f9e1e3e7a0f0950ce
Adithya Sripada <166884730+Adithya-Sripada@users.noreply.github.com>
2026-05-29 12:56:26 -0700

    Update NumberOfOne.cpp

--- NumberOfOne.cpp
+++ NumberOfOne.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
-#include <string>
 #include <cmath>
 
 int countDigitOne(int n) {
     if(n <= 0) return 0;
     if(n <= 9) return 1;
-    int x = n;
+    int copy = n;
     int digits = 0;
     int count = 0;
-    while(x > 0) {
-        int r = x%10;
+    int nine = 0;
+    while(copy > 0) {
+        int r = copy%10;
         if(r == 1) count++;
-        x = x/10;
+        copy = copy/10;
+        nine = nine*10 + 9;
         digits++;
     }
 
-    std::string nine(digits, '9');
-    if(std::to_string(n) == nine) return digits * pow(10, digits-1);
+    if(n == nine) return digits * pow(10, digits-1);
     else return count + countDigitOne(n-1);
 }
