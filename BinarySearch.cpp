1b8a4fe9406fd98a914812077d17f41d8e3ae7a3
Adithya Sripada <166884730+Adithya-Sripada@users.noreply.github.com>
2026-07-05 22:40:57 -0700

    Update BinarySearch.cpp

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -3,6 +3,8 @@
         int mid = nums.size()/2;
         int left = 0;
         int right = nums.size()-1;
+        if(nums[left] == target) return left;
+        if(nums[right] == target) return right;
         do {
             if(nums[mid] < target) {
                 left = mid;
