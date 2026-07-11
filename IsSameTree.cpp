d0c6866f2bad17f7188a7b4d02a5f10259934ec6
Adithya Sripada <166884730+Adithya-Sripada@users.noreply.github.com>
2026-07-10 17:49:11 -0700

    Create IsSameTree.cpp

---
+++ IsSameTree.cpp
@@ -0,0 +1,15 @@
+struct TreeNode {
+   int val;
+   TreeNode *left;
+   TreeNode *right;
+   TreeNode() : val(0), left(nullptr), right(nullptr) {}
+   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+bool isSameTree(TreeNode* p, TreeNode* q) {
+    if(!p && !q) return true;
+    if(p == nullptr xor q == nullptr) return false;
+
+    bool t = p->val == q->val;
+    return t && (isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
+}
